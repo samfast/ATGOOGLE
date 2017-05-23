7bf0da72a772b9dcef824a264ab10f458cec15ab
samuel kyama <samfastone@gmail.com>
2017-05-23 06:46:16 +0300

    final commit befor submission

--- cake.py
+++ cake.py
@@ -1,35 +1,49 @@
-import Test
 
-def answer(cake):
-    cakeSet=set(cake);
-    print(cakeSet)
-    cakeDict={}
-    for key in cakeSet:
-        cakeDict[key]=cake.count(key)
+
+def answer(s):
+    sSet=set(s);
+    check=0
+    print(sSet)
+    sDict={}
+    for key in sSet:
+        sDict[key]=s.count(key)
     minValue=1000000
-    for k, v in cakeDict.items():
-        if v < minValue:
+    for k, v in sDict.items():
+        if v < minValue and len(s)%v==0 and v!=1000000:
             minValue=v
+    while minValue==  1000000:
+        print('this cake can only be given to 1 minion')
+        actualPeople=1
+        return actualPeople
     maxPeople=minValue
-    print(maxPeople,"\n")
+    print(maxPeople,"are the max people")
 
     for actualPeople in range(maxPeople):
         actualPeople=maxPeople-actualPeople
-        print(actualPeople)
+        #print(actualPeople)
+        print('the length of s is',len(s))
+        checkSize=len(s)/maxPeople
+        for test in range(0,len(s),checkSize):
+            firstSlice=str(s[0:checkSize])
+            print('the firstSlice is ',firstSlice)
+            ntestsize=test+checkSize
+            testbeg=test
+            teststring=str(s[testbeg:ntestsize])
+            print('the teststring is',teststring)
+            if  firstSlice!=teststring:
+                print('fail for division btn',test)
+                break
+            else:
+                print ('s is divisible among',test)
+                check=test
+                print(check)
 
-        checkSize=len(cake)/maxPeople
-
-        for test in range(0,len(size),checkSize):
-            firstSlice=cake[0:checkSize]
-            if  firstSlice!=cake([test:test+checkSize])
-                
+        print('this are the actualPeople',actualPeople)
+        return actualPeople
 
 
+answer("bababababa")
 
-
-
-
-print("bacdaabacdaa"[0:3])
 
 
 #Test.assert_equals(answer(list("babaababaa")), 2)
